657a22f8321e3fb411ec67c05e454927a4aa1510
akkk12 <68320786+akkk12@users.noreply.github.com>
2020-07-21 17:03:24 +0530

    Add files via upload

---
+++ webpage.js
@@ -0,0 +1,26 @@
+function myfunc()
+{
+	var x=document.getElementById("navigation");
+	if(x.style.display=="inline-block"){
+		x.style.display="none";
+		
+
+	}
+	else
+		x.style.display="inline-block";
+
+}
+
+function myfunc2()
+{
+	var x=document.getElementById("forms");
+	if(x.style.display=="block"){
+		x.style.display="none";
+		
+
+	}
+	else
+		x.style.display="block";
+
+}
+
